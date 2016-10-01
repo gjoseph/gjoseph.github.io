06ec71d840547d775eaf9cf30d103120e5f65174
Gregory Joseph <gjoseph@atlassian.com>
2016-10-01 21:42:51 +1000

    Updated setup
    * .idea in .gitignore
    * notes in ./test-site.sh
    * Gemfile update to copy latest (iunderstandnothing)

--- test-site.sh
+++ test-site.sh
@@ -1,4 +1,11 @@
-jekyll serve &
+#!/usr/bin/env bash
+
+# sudo gem install bundler
+# sudo gem install nokogiri -v '1.6.8' -- --use-system-libraries
+# bundler install
+# what a bunch of crock
+
+bundle exec jekyll serve &
 sleep 2
 open http://localhost:4000
 wait
